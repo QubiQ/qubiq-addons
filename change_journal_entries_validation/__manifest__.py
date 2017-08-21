7ee3809e18d3e5fe012635bc0690a4de4ad2f033
valentin vinagre urteaga <valentin.vinagre@qubiq.es>
2017-08-21 10:34:30 +0200

    README y change_journal_entries_validation

---
+++ change_journal_entries_validation/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (c) 2017 QubiQ (http://www.qubiq.es)
+
+{
+    "name": "Change journal entries validation",
+    "version": "10.0.1.0.0",
+    "author": "QubiQ, Odoo Community Association (OCA)",
+    "website": "https://www.qubiq.es",
+    "category": "Account",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "account",
+        "account_accountant",
+        "account_cancel",
+    ],
+    "data": [
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    'installable': True,
+    'application': True,
+}
